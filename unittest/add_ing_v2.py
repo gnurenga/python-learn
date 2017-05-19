0cead4df6f974d4fe8dba052b2515a727ad2a8c0
Rengaraj <sakthirengaraj@gmail.com>
2017-05-19 17:33:00 +0530

    Fixed coding style as per pep8

--- unittest/add_ing_v2.py
+++ unittest/add_ing_v2.py
@@ -1,17 +1,12 @@
 def isVerbends_with_e(word):
     if len(word) > 3:
-
         if word[-1] == 'e':
-
             return True
-
     else:
-
         return False
 
 
 def isVerbConsonent(word):
-
     vowels = "aeiou"
 
     if len(word) != 3:
@@ -19,33 +14,17 @@ def isVerbConsonent(word):
 
     if word[0] not in vowels and word[2] not in vowels:
         if word[1] in vowels:
-
             return True
     else:
-
         return False
 
 
 def add_ing(word):
 
     if isVerbends_with_e(word):
-
         return word[:-1] + "ing"
 
     if isVerbConsonent(word):
-
         return word + word[-1] + "ing"
 
     return word + "ing"
-
-
-
-
-
-# words = ["shave", "comb", "make", "feed", "hope", "save", "tap"]
-
-
-
-# for word in words:
-
-#     print add_ing(word)
