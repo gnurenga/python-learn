d8b3f3e0990b914eaeabf65c13020d7343ac8945
Rengaraj <sakthirengaraj@gmail.com>
2018-10-08 01:48:43 -0700

    prompt toolkit learning, first example to show the prompt and get the input

---
+++ cli/prompt_1.py
@@ -0,0 +1,9 @@
+from __future__ import unicode_literals
+from prompt_toolkit import prompt
+
+def main():
+    text = prompt('|--> ')
+    print 'You entered:', text
+    
+if __name__ == '__main__':
+    main()
